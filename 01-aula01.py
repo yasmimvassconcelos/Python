1d4f053c2fa63f2307894dbb52a5db9dde2c70f0
Yasmim Letícia <yasmimleticia859@gmail.com>
2024-08-28 17:39:44 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Yasmim") # comentário a linha do código
+print('Yasmim')
+print('Yasmim Letícia')
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
+
