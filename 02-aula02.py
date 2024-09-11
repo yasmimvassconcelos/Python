5cc32d9fbaccdc2ce3ce21121c69ff95ea3b7fde
Yasmim Letícia <yasmimleticia859@gmail.com>
2024-09-11 17:40:41 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Yasmim"
+idade = 16
+altura = 1.72
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade ", idade, "altura: ", altura)
+# formção com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
