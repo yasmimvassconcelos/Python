ab9c81db7ee110abee32b55cf7d943ad21bb99a4
Yasmim Letícia <yasmimleticia859@gmail.com>
2024-10-02 16:52:11 -0300

    Add files via upload

---
+++ aula10-tabuada.py
@@ -0,0 +1,22 @@
+# operadores aritmeticos
+#soma
+soma = 10+5
+print(soma)
+#subtracao
+subtracao = 10 - 5
+print(subtracao)
+# multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+#divisao
+divisao = 10 / 5
+print(divisao)
+# divisao inteira
+divisao_inteira = 10 // 5
+print(divisao_inteira)
+#exponenciacao
+exponenciacao = 10 ** 2 
+print(exponenciacao)
+#modulo / resto da divisao
+modulo = 10 % 5
+print(modulo)
